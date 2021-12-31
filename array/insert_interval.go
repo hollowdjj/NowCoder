858a6a0045402fe6a0bff52800a4d43bf76d1e69
hollowdjj <1187636437@qq.com>
2021-12-31 16:23:23 +0800

    插入区间

---
+++ array/insert_interval.go
@@ -0,0 +1,12 @@
+package array
+
+/*
+给定一个无重叠的，按照区间起点升序排列的区间列表，在列表中插入一个新区间，如果有原区间有重合，则合并，请返回插入后的区间列表。
+
+输入：[[2,5],[6,11]],[5,6]
+输出：[[2,11]]
+*/
+
+func InsertInterval(Intervals []*Interval, newInterval *Interval) []*Interval {
+	return nil
+}
