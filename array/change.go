d800cb09dcdbd1566b88630cd80b4a6549234670
hollowdjj <1187636437@qq.com>
2022-01-05 15:34:13 +0800

    兑换硬币

---
+++ array/change.go
@@ -0,0 +1,14 @@
+package array
+
+/*
+给定一个整数数组 nums 表示不同数额的硬币和一个正整数 target 表示总金额，请你计算
+并返回可以凑出总金额的的组合数。如果凑不出 target 则返回 0。
+
+输入：  5,[1,2,4,5]
+返回值：5
+*/
+
+func Change(target int, nums []int) int {
+	//dp[i][j]表示利用从i到最后一种面额的硬币，凑j能有多少种方案
+	return 1
+}
