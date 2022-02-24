1a062043d3ac201e4fe101cdcb8d881b26f8627f
hollowdjj <1187636437@qq.com>
2022-02-24 18:16:30 +0800

    最大差值

---
+++ others/max_difference.go
@@ -0,0 +1,17 @@
+package others
+
+/*
+最大差值
+有一个长为 n 的数组 A ，求满足 0 ≤ a ≤ b < n 的 A[b] - A[a] 的最大值。
+给定数组 A 及它的大小 n ，请返回最大差值。
+*/
+
+func MaxDifference(A []int, n int) int {
+	res := 0
+	minVal := A[0]
+	for i := 1; i < n; i++ {
+		minVal = min(minVal, A[i]) //A[0..i]中的最小值
+		res = max(res, A[i]-minVal)
+	}
+	return res
+}
