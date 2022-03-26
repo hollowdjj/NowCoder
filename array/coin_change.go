1858db3027ee4a14d86b18bfbd50f4c3764fb035
hollowdjj <1187636437@qq.com>
2022-03-26 22:34:35 +0800

    零钱兑换

--- array/coin_change.go
+++ array/coin_change.go
@@ -53,3 +53,35 @@ func ChangeDp(target int, nums []int) int {
 
 	return dp[n-1][target]
 }
+
+/*
+给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+计算并返回可以凑成总金额所需的最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回-1 。
+你可以认为每种硬币的数量是无限的。
+*/
+
+func CoinChange(coins []int, amount int) int {
+	n := len(coins)
+
+	//dp[i]表示使用coins凑出金额i所需的最少硬币数。那么状态转移方程为：
+	//dp[i] = Min{dp[i-coins[j]]}，其中0<=j<len(coins)-1
+	//dp[0] = 0
+	dp := make([]int, amount+1)
+	for i := 1; i <= amount; i++ {
+		dp[i] = amount + 1
+	}
+
+	for i := 1; i <= amount; i++ {
+		for j := 0; j < n; j++ {
+			if i < coins[j] {
+				continue
+			}
+			dp[i] = min(dp[i], dp[i-coins[j]]+1)
+		}
+	}
+
+	if dp[amount] == amount+1 {
+		return -1
+	}
+	return dp[amount]
+}
