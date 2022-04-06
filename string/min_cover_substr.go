bf7f589a727d3d2442fba8f3ac3c111b32609b9c
hollowdjj <1187636437@qq.com>
2022-04-06 14:08:50 +0800

    最小覆盖子串

--- string/min_cover_substr.go
+++ string/min_cover_substr.go
@@ -1,5 +1,7 @@
 package string
 
+import "math"
+
 /*
 最小覆盖子串
 给出两个字符串 s 和 t，要求在 s 中找出最短的包含 t 中所有字符的连续子串。
@@ -13,54 +15,52 @@ S ="XDOYEZODEYXNZ" T ="XYZ"
 满足条件的子串可能有很多，但是题目保证满足条件的最短的子串唯一。
 */
 
-func MinWindow(S string, T string) string {
-	ns, nt := len(S), len(T)
-	if ns < nt || nt == 0 {
-		return ""
-	}
-	//先使用一张哈希表记录T中的所有字符出现的次数
-	dic := make(map[byte]int)
-	count := 0
+func MinWindow(s string, t string) string {
+	need := make(map[byte]int)
+	nt, ns := len(t), len(s)
 	for i := 0; i < nt; i++ {
-		if dic[T[i]] == 0 {
-			count++
-		}
-		dic[T[i]]++
+		need[t[i]]++
 	}
-
-	//使用滑动窗口遍历字符串S
-	left, right := 0, 0
-	start, end := -1, ns
+	count := len(need)
+	left, right := 0, 0 //滑动窗口左右指针
+	start, length := 0, math.MaxInt32
 	window := make(map[byte]int)
-	match := 0
 	for right < ns {
-		c := S[right]
-		if dic[c] > 0 {
+		//滑动窗口right指针一直右移，直到滑动窗口中包含了所有t中的字符
+		for right < ns {
+			c := s[right]
 			window[c]++
-			if window[c] == dic[c] {
-				//说明对于T中的所有字符c都能够在S[left,right]中找到，匹配度加1
-				match++
+			if window[c] == need[c] {
+				count--
+				if count == 0 {
+					break
+				}
+			}
+			right++
+		}
+
+		if count == 0 {
+			//往右移动滑动窗口的左指针，直到滑动窗口中的元素刚好覆盖t中的字符，然后更新结果
+			stop := false
+			for left <= right && !stop {
+				c := s[left]
+				if window[c] == need[c] {
+					//窗口无法缩小了，更新答案
+					if right-left+1 < length {
+						start = left
+						length = right - left + 1
+					}
+					stop = true
+					count++
+				}
+				window[c]--
+				left++
 			}
 		}
 		right++
-		//这里一定是for循环。因为left++不一定会导致match--。所以我们要一直更新解，直到math != count
-		for match == count {
-			//T中的所有字符已经在S[left,right]中找到，更新解
-			if right-left < end-start {
-				start, end = left, right
-			}
-			c1 := S[left]
-			if dic[c1] > 0 {
-				window[c1]--
-				if window[c1] < dic[c1] {
-					match--
-				}
-			}
-			left++
-		}
 	}
-	if start == -1 {
+	if length == math.MaxInt32 {
 		return ""
 	}
-	return S[start:end]
+	return s[start : length+start]
 }
