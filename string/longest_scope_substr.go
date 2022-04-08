84ad71739c6aba56326638b4af6aed9f2a9f7745
hollowdjj <1187636437@qq.com>
2022-04-08 11:07:13 +0800

    最长有效括号子串长度

--- string/longest_scope_substr.go
+++ string/longest_scope_substr.go
@@ -17,8 +17,7 @@ func LongestValidParentheses(s string) int {
 	//当s[i] = ')' && s[i-1] == ')时：
 	// 形如  ...(      (....)     )
 	//     i-1-dp[i-1]     i-1    i
-	//如果s[i-1-dp[i-1]]为')'的话dp[i] = dp[i-1] + 2
-	//如果s[i-1-dp[i-1]]为'('的话dp[i] = dp[i-1]+dp[i-1-dp[i-1-1]+2
+	//如果s[i-1-dp[i-1]]为'('的话dp[i] = dp[i-1]+dp[i-1-dp[i-1]-1]+2
 	n := len(s)
 	dp := make([]int, n)
 	res := 0
@@ -44,4 +43,48 @@ func LongestValidParentheses(s string) int {
 		res = max(res, dp[i])
 	}
 	return res
+}
+
+func longestValidParentheses(s string) int {
+	//在此方法中，我们利用两个计数器left和right 。首先，我们从左到右遍历字符串，对于遇到的每个‘(’，我们
+	//增加left计数器，对于遇到的每个‘)’，我们增加right计数器。每当left计数器与right计数器相等时，我们计算
+	//当前有效字符串的长度，并且记录目前为止找到的最长子字符串。当right计数器比left 计数器大时，我们将left
+	//和right计数器同时变回0。
+
+	//这样的做法贪心地考虑了以当前字符下标结尾的有效括号长度，每次当右括号数量多于左括号数量的时候之前的字符
+	//我们都扔掉不再考虑，重新从下一个字符开始计算，但这样会漏掉一种情况，就是遍历的时候左括号的数量始终大于
+	//右括号的数量，即 (() ，这种时候最长有效括号是求不出来的。
+	//解决的方法也很简单，我们只需要从右往左遍历用类似的方法计算即可，只是这个时候判断条件反了过来：
+
+	//当left计数器比right计数器大时，我们将left和right计数器同时变回0
+	//当left计数器与right计数器相等时，我们计算当前有效字符串的长度，并且记录目前为止找到的最长子字符串
+	//这样我们就能涵盖所有情况从而求解出答案。
+
+	left, right, maxLength := 0, 0, 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left == right {
+			maxLength = max(maxLength, 2*right)
+		} else if right > left {
+			left, right = 0, 0
+		}
+	}
+	left, right = 0, 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left == right {
+			maxLength = max(maxLength, 2*left)
+		} else if left > right {
+			left, right = 0, 0
+		}
+	}
+	return maxLength
 }
