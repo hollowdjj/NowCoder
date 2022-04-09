d989f506762e274bd10f9a4830305b3fc04b363e
hollowdjj <1187636437@qq.com>
2022-04-09 16:06:33 +0800

    链表排序

--- list/sort_list.go
+++ list/sort_list.go
@@ -1,58 +1,88 @@
 package list
 
-import "nowcoder/utility"
+import (
+	"math"
+)
 
 /*
 给定一个节点数为n的无序单链表，对其按升序排序。
 要求：空间复杂度 O(n)，时间复杂度 O(nlogn)
 */
 
-//SortInList 升序排序列表元素
-func SortInList(head *utility.ListNode) *utility.ListNode {
-	//使用一个额外的数组存储链表的所有值，即空间复杂度为O(n)
-	var array []int
+func SortList(head *ListNode) *ListNode {
+	return mergeSort(head)
+}
+
+func mergeSort(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	prev, mid := findMid(head)
+	prev.Next = nil //断链
+	return mergeTowList(mergeSort(head), mergeSort(mid))
+}
+
+func mergeTowList(l1, l2 *ListNode) *ListNode {
+	dummy := &ListNode{Val: -1}
+	nHead := dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			nHead.Next = l1
+			l1 = l1.Next
+		} else {
+			nHead.Next = l2
+			l2 = l2.Next
+		}
+		nHead = nHead.Next
+	}
+
+	if l1 != nil {
+		nHead.Next = l1
+	} else {
+		nHead.Next = l2
+	}
+	return dummy.Next
+}
+
+func findMid(head *ListNode) (*ListNode, *ListNode) {
+	slow, fast := head, head
+	var prev *ListNode
+	for fast != nil && fast.Next != nil {
+		prev = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return prev, slow
+}
+
+func sortList(head *ListNode) *ListNode {
+	//链表的插入排序
+	dummy := &ListNode{Val: math.MinInt32}
+	tail := dummy
+
 	for head != nil {
-		array = append(array, head.Val)
-		head = head.Next
-	}
-	//对这个数组使用归并排序，即时间复杂度为O(nlogn)
-	array = MergeSort(array)
-	return utility.SliceToList(array)
-}
+		//将head从原链表中断开
+		next := head.Next
+		head.Next = nil
 
-//对两已排序的数组中的元素进行排序并返回排序后的数组
-func mergeArray(left, right []int) []int {
-	n1, n2 := len(left), len(right)
-	temp := make([]int, n1+n2)
-	j, k := 0, 0
-	for i := 0; i < n1+n2; i++ {
-		for j < n1 && k < n2 {
-			if left[j] <= right[k] {
-				temp[i] = left[j]
-				j++
-			} else {
-				temp[i] = right[k]
-				k++
+		if head.Val < tail.Val {
+			//插入排序。从dummy后面开始找到第一个大于head.Val的节点，然后将head插入到它前面
+			prev := dummy
+			nHead := dummy.Next
+			for nHead.Val < head.Val {
+				prev = nHead
+				nHead = nHead.Next
 			}
-			i++
-		}
-		//如果arr1或arr2还没有遍历完，直接添加
-		if j < n1 {
-			temp[i] = left[j]
-			j++
-		}
-		if k < n2 {
-			temp[i] = right[k]
-			k++
-		}
-	}
-	return temp
-}
 
-func MergeSort(slice []int) []int {
-	if slice == nil || len(slice) < 2 {
-		return slice
+			//将head挂载到prev后面
+			prev.Next = head
+			head.Next = nHead
+		} else {
+			tail.Next = head
+			tail = tail.Next
+		}
+		head = next
 	}
-	mid := len(slice) / 2
-	return mergeArray(MergeSort(slice[0:mid]), MergeSort(slice[mid:]))
+	return dummy.Next
 }
