48558e5d877c03e7c2addf06361a83f40824ab2f
hollowdjj <1187636437@qq.com>
2022-04-10 11:02:27 +0800

    非递归中序遍历

--- tree/inorder.go
+++ tree/inorder.go
@@ -18,3 +18,30 @@ func Inorder(root *TreeNode) []int {
 	inorder(root)
 	return res
 }
+
+//非递归实现中序遍历
+func inorderTraversal(root *TreeNode) []int {
+	res := make([]int, 0)
+	stack := make([]*TreeNode, 0)
+	for root != nil || len(stack) > 0 {
+		//一直走左子树直到叶子节点
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		//          1
+		//      2       3
+		//   4     5
+		//例如，得到叶子节点4，打印值，然后处理它的右子节点
+		//右子节点为空，所以不会走上面那个while循环。然后打
+		//印2，并处理2的右子树。
+		if len(stack) > 0 {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res = append(res, top.Val) //根节点
+			root = top.Right
+		}
+	}
+	return res
+}
