92e560beca7a342f303de794a83d09babfebce07
hollowdjj <1187636437@qq.com>
2022-04-18 16:58:54 +0800

    二叉搜索树的后续遍历序列

---
+++ jz/jz33_二叉搜索数的后续遍历序列.go
@@ -0,0 +1,29 @@
+package jz
+
+import "math"
+
+/*
+输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则返回 true ,否则返回 false 。
+假设输入的数组的任意两个数字都互不相同。
+*/
+
+func VerifySequenceOfBST(postorder []int) bool {
+	if len(postorder) == 0 {
+		return false
+	}
+	//后序遍历顺序是：左右根，倒序遍历后续遍历数组即：根右左
+	//那么，将根节点和右节点的值入栈
+	stack := make([]int, 0)
+	root := math.MaxInt32
+	for i := len(postorder) - 1; i >= 0; i-- {
+		if postorder[i] > root {
+			return false
+		}
+		for len(stack) > 0 && postorder[i] < stack[len(stack)-1] {
+			root = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, postorder[i])
+	}
+	return true
+}
