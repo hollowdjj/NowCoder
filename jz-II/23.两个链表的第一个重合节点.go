6fc22d36e91537dda4b96b14527486e987cef888
hollowdjj <1187636437@qq.com>
2022-06-07 11:09:05 +0800

    1

---
+++ jz-II/23.两个链表的第一个重合节点.go
@@ -0,0 +1,18 @@
+package jz_II
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	pA, pB := headA, headB
+	for pA != pB {
+		if pA == nil {
+			pA = headB
+		} else {
+			pA = pA.Next
+		}
+		if pB == nil {
+			pB = headA
+		} else {
+			pB = pB.Next
+		}
+	}
+	return pA
+}
--- jz-II/utility.go
+++ jz-II/utility.go
@@ -1,1 +1,6 @@
 package jz_II
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
