dc5b9fd853fa108b55b04a174f682cbe7b466012
hollowdjj <1187636437@qq.com>
2022-06-07 15:11:09 +0800

    哈希表

---
+++ jz-II/32.有效的变位词.go
@@ -0,0 +1,25 @@
+package jz_II
+
+/*
+给定两个字符串 s 和 t ，编写一个函数来判断它们是不是一组变位词（字母异位词）。
+注意：若s和t中每个字符出现的次数都相同且字符顺序不完全相同，则称s和t互为变位词（字母异位词）。
+
+输入: s = "anagram", t = "nagaram"
+输出: true
+*/
+func isAnagram(s string, t string) bool {
+	if s == t {
+		return false
+	}
+	dic := make(map[rune]int)
+	for _, r := range s {
+		dic[r]++
+	}
+	for _, r := range t {
+		dic[r]--
+		if dic[r] < 0 {
+			return false
+		}
+	}
+	return true
+}
