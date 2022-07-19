d3acc9e2c3859dc279a2417ccbe0f86d4abcd9fd
daijunjie <1187636437@qq.com>
2022-07-19 23:50:00 +0800

    图的连通量

---
+++ jz-II/116.省份数量.go
@@ -0,0 +1,35 @@
+package jz_II
+
+/*
+有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，
+那么城市 a 与城市 c 间接相连。
+
+省份是一组直接或间接相连的城市，组内不含其他没有相连的城市。
+
+给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接
+相连，而 isConnected[i][j] = 0 表示二者不直接相连。
+
+返回矩阵中 省份 的数量。
+*/
+
+func findCircleNum(isConnected [][]int) int {
+	n := len(isConnected)
+	visit := make([]bool, n)
+	var dfs func(start int)
+	dfs = func(start int) {
+		visit[start] = true
+		for to, conn := range isConnected[start] {
+			if conn == 1 && !visit[to] {
+				dfs(to)
+			}
+		}
+	}
+	res := 0
+	for i, v := range visit {
+		if !v {
+			res++
+			dfs(i)
+		}
+	}
+	return res
+}
