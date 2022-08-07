d3e6fbd2097615f769946a89801df9c1e60c76b2
daijunjie <1187636437@qq.com>
2022-08-07 17:51:45 +0800

    滑动窗口

---
+++ jz-II/17.含有所有字符的最短子串.go
@@ -0,0 +1,51 @@
+package jz_II
+
+/*
+给定两个字符串 s 和 t 。返回 s 中包含 t 的所有字符的最短子字符串。如果 s 中不存在符合
+条件的子字符串，则返回空字符串 ""。
+
+如果 s 中存在多个符合条件的子字符串，返回任意一个。
+
+输入：s = "ADOBECODEBANC", t = "ABC"
+输出："BANC"
+解释：最短子字符串 "BANC" 包含了字符串 t 的所有字符 'A'、'B'、'C'
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/M1oyTv
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func minWindow(s string, t string) string {
+	if len(t) > len(s) {
+		return ""
+	}
+	dic := make(map[byte]int)
+	for i := range t {
+		dic[t[i]]++
+	}
+	win := make(map[byte]int)
+	res := ""
+	left, right := 0, 0
+	count := 0
+	for right < len(s) {
+		b := s[right]
+		win[b]++
+		if win[b] == dic[b] {
+			count++
+		}
+		//缩小滑动窗口，找最短子串
+		for count == len(dic) {
+			if res == "" || len(res) > right-left+1 {
+				res = s[left : right+1]
+			}
+			win[s[left]]--
+			if win[s[left]] < dic[s[left]] {
+				count--
+			}
+			left++
+		}
+		right++
+	}
+
+	return res
+}
