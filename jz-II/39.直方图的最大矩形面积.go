f70de93fae4325953ea3f799062b50f6b4d85225
daijunjie <1187636437@qq.com>
2022-08-09 22:29:22 +0800

    单调栈

--- jz-II/39.直方图的最大矩形面积.go
+++ jz-II/39.直方图的最大矩形面积.go
@@ -10,7 +10,8 @@ func largestRectangleArea(heights []int) int {
 			i++
 		} else {
 			//此时，对于栈顶元素而言，i为其右边第一个高度大于它的矩阵的索引
-			//而其下方的元素就是左边第一个高度小于他的元素的索引
+			//而其下方的元素就是左边第一个高度小于等于他的元素的索引，所以这里
+			//没有i++，也是因为下方元素的高度可能等于栈顶元素的高度。
 			top := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 			width := 0
