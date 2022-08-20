8fdb5a7bca1dbbfa7acbc94f7d7a08b8e141c71e
hollowdjj <1187636437@qq.com>
2022-08-20 17:23:55 +0800

    迭代+递归

--- jz-II/24.反转链表.go
+++ jz-II/24.反转链表.go
@@ -1,6 +1,13 @@
 package jz_II
 
+/*
+给定链表头节点，翻转链表。
+输入：1 2 3 4 5
+输出：5 4 3 2 1
+*/
+
 func reverseList(head *ListNode) *ListNode {
+	//迭代法翻转链表
 	var prev *ListNode
 	for head != nil {
 		next := head.Next
@@ -12,6 +19,7 @@ func reverseList(head *ListNode) *ListNode {
 }
 
 func reverseList1(head *ListNode) *ListNode {
+	//递归法翻转链表
 	var dfs func(prev, curr *ListNode) *ListNode
 	dfs = func(prev, curr *ListNode) *ListNode {
 		if curr == nil {
